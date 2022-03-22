1a5de785bc71178ecf3d959e2561fa3d8f07f825
Dan <dannybrown2710@gmail.com>
2022-03-23 01:26:59 +0530

    restaurant

--- Menu/controller/index.js
+++ Menu/controller/index.js
@@ -1,6 +1,6 @@
 const Restaurant = require('../../Restaurant/model')
 const getMenu = async(req,res) => {
-    const restro = await Restaurant.findOne({_id:req.params.id});
+    const restro = await Restaurant.findOne({_id:req.params.restaurantId});
     res.status(200).send({menu: restro.menu} );
 }
 module.exports = {
